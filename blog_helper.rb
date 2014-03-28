6fd90d092262ebb177966a0ade600fd8e5c30581
Phodal <gmszone@qq.com>
2014-03-28 21:21:27 +0800

    add for the image response

--- blog_helper.rb
+++ blog_helper.rb
@@ -9,9 +9,10 @@ class BlogHelper
     response = Net::HTTP.get_response("localhost","/?wpapi=search&dev=1&keyword="+query)
     posts = (JSON.parse response.body)['posts']
     posts.each do |post|
-      post_id = posts['id']
+      post_id = post['id']
       image_response = Net::HTTP.get_response("localhost","/?wpapi=get_posts&dev=1&comment=1&content=1&id="+post_id)
-      image_req = Nokogiri::HTML((JSON.parse image_response.body)['posts']['content'].css('img').first['src']
+      image_response_content = (JSON.parse image_response.body)['posts'][0]['content']
+      image_req = Nokogiri::HTML(image_response_content).css('img').first['src']
       result << {
         :title => post['title'],
         :description => /&hellip;/.match(post['excerpt']).pre_match,
