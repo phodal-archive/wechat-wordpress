7c1425b964304e08e63058fa2dd56902cf09d62a
Phodal <gmszone@qq.com>
2014-03-28 20:50:06 +0800

    add the image for wechat

--- blog_helper.rb
+++ blog_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'json'
 require 'net/http'
+require 'nokogiri'
 
 class BlogHelper
   def getdata(query)
@@ -8,10 +9,13 @@ class BlogHelper
     response = Net::HTTP.get_response("localhost","/?wpapi=search&dev=1&keyword="+query)
     posts = (JSON.parse response.body)['posts']
     posts.each do |post|
+      post_id = posts['id']
+      image_response = Net::HTTP.get_response("localhost","/?wpapi=get_posts&dev=1&comment=1&content=1&id="+post_id)
+      image_req = Nokogiri::HTML((JSON.parse image_response.body)['posts']['content'].css('img').first['src']
       result << {
         :title => post['title'],
         :description => /&hellip;/.match(post['excerpt']).pre_match,
-        :picture_url => post['author'][0]['gravatar'],
+        :picture_url => image_req,
         :url => post['url']
       }
     end
