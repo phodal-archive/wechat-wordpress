ad125230bd0136e501216a4e46b9fff384994b9a
Phodal <gmszone@qq.com>
2014-03-29 20:58:27 +0800

    move to the default image configure

--- blog_helper.rb
+++ blog_helper.rb
@@ -6,13 +6,17 @@ require 'nokogiri'
 class BlogHelper
   def getdata(query)
     result = []
-    response = Net::HTTP.get_response("localhost","/?wpapi=search&dev=1&keyword="+query)
+    response = Net::HTTP.get_response("xunta.phodal.net","/?wpapi=search&dev=1&keyword="+query)
     posts = (JSON.parse response.body)['posts']
-    posts.each do |post|
-      post_id = post['id']
-      image_response = Net::HTTP.get_response("localhost","/?wpapi=get_posts&dev=1&comment=1&content=1&id="+post_id)
-      image_response_content = (JSON.parse image_response.body)['posts'][0]['content']
-      image_req = Nokogiri::HTML(image_response_content).css('img').first['src']
+    posts.each do |post,index|
+      if index = 1
+        post_id = post['id']
+        image_response = Net::HTTP.get_response("xunta.phodal.net","/?wpapi=get_posts&dev=1&comment=1&content=1&id="+post_id)
+        image_response_content = (JSON.parse image_response.body)['posts'][0]['content']
+        image_req = Nokogiri::HTML(image_response_content).css('img').first['src']
+      else
+        image_req = 'http://www.gravatar.com/avatar/56d54d4b6b4dc4e3efa458f745d61513?s=100&d=mm&r=g'
+      end 
       result << {
         :title => post['title'],
         :description => /&hellip;/.match(post['excerpt']).pre_match,
@@ -20,6 +24,10 @@ class BlogHelper
         :url => post['url']
       }
     end
+    p result
     result
   end
 end
+
+blog = BlogHelper.new
+blog.getdata('th')
